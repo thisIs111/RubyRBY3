adee3ea8d42466ccd9684c26727c8adb1b498b27
111 <110096164+thisIs111@users.noreply.github.com>
2022-07-28 18:35:10 +0700

    Add files via upload

---
+++ ruby2/page8/index.rb
@@ -0,0 +1,8 @@
+# Rewrite the below hash using the shorthand syntax
+exam = {subject: "Math", score: 80}
+
+puts "#{exam[:subject]} score is #{exam[:score]}"
+
+# exam = {subject: "Math", score: 80}
+
+# puts "#{exam[:subject]} score is #{exam[:score]}"
